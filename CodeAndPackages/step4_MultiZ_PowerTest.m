cc9ca92e9321f4756b64b47086d065de2899fc04
gmbcrazy <math2437@hotmail.com>
2025-09-23 17:09:34 -0400

    update from imaging PC

--- CodeAndPackages/step4_MultiZ_PowerTest.m
+++ CodeAndPackages/step4_MultiZ_PowerTest.m
@@ -3,7 +3,7 @@ clear all
 ConfigFolder='C:\Users\User\Project-SLMonlineControl\config\';
 
 
-WorkingFolder='E:\LuSLMOnlineTest\SL1968\08052025\'
+WorkingFolder='E:\LuSLMOnlineTest\L00121\09232025\'
 % load('C:\Users\zhangl33\Projects\Project-SLMonlineControl\subfun\Color\colorMapPN3.mat');
 load('C:\Users\User\Project-SLMonlineControl\subfun\Color\colorMapPN3.mat');
 confSet = ReadYaml([WorkingFolder 'CurrentSLMsetting.yml']);
@@ -101,9 +101,9 @@ FileType=2;   %Choose a specific bin file as reference for motion correction
 % 
 FileType=0;   %Choose a pre-recorded multi-tif files for motion correction
 RefFile=[];
-RefFile=[WorkingFolder 'RegRef1Ch1Ch2\'];
-[RegOps, RegImg] = LoadRegRefFile(RefFile, FileType,numGPUs);
-RegImg=RegImg(:,:,4:6);
+% RefFile=[WorkingFolder 'RegRef1Ch1Ch2\'];
+% [RegOps, RegImg] = LoadRegRefFile(RefFile, FileType,numGPUs);
+% RegImg=RegImg(:,:,4:6);
 
 RefFile=[WorkingFolder 'RegRef2Ch2\'];
 [RegOps, RegImg] = LoadRegRefFile(RefFile, FileType,numGPUs);
@@ -132,9 +132,9 @@ step4_MultiZ_SubStep1_PreTest  %% generate next points being test and update xml
 % XMLparam.PointList=[7 9 7 16 17 7 21 22 17 30];
 % XMLparam.Laser(1:10)=[1.35 1.35 1.35 1.35 1.55 1.35 1.6 1.55 1.55 1.6];
 % XMLparam.RoundID=randperm(XMLparam.TotalRounds,1);
-XMLparam.PointList=[2 3 27 14 30 2 3 27 14 30];
+XMLparam.PointList=[6  37  1     28    13     6    14     7   1  6];
 % % % % % % % % % XMLparam.PointList(10)=[29]
-XMLparam.Laser(1:10)=repmat(1.52,1,10);
+XMLparam.Laser(1:10)=repmat(1.5,1,10);
 % % 
 pause(10)
 
@@ -155,7 +155,7 @@ SLMTableOrigin=SLMTable;
 FalsePositiveID=input('Mannual correction: index in SLMTable with false positive error: ');
 SLMTable(FalsePositiveID,2)=nan;
 
-SLMTable([22],2)=1.52;
+SLMTable([37],2)=1.5;
 % SLMTable([26 27 28 34],2)=1.5;
 
 
